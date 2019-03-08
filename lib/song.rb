53500f29fe834763615afa04f62fcedb5985327f
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-08 19:19:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(filename)
     song = Song.new(filename.split("-")[1].strip)
     binding.pry
-    song.artist = Artist.new(filename.split("-")[0].strip)
+    new_artist = Artist.new(filename.split("-")[0].strip)
     # song.artist() = (filename.split("-")[0].strip)
     # song.artist.name = filename.split("-")[0].strip
   end
