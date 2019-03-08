f8877ffa82df253ce81066dfba87e54154146259
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-08 18:57:21 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,9 +4,9 @@ class MP3Importer
 
   def initialize(path)
     @path = path ##"./spec/fixtures/mp3s"
-    binding.pry
     @files = []
     Dir[@path].each {|file| @files << file}
+      binding.pry
   end
 
   def import
