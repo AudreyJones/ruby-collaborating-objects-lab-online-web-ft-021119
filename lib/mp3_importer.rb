16609f4f30d6f2cc6fbde754239a1d8acf8d166f
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-08 19:06:59 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,10 @@ class MP3Importer
     @path = path ##"./spec/fixtures/mp3s"
     @files = []
   # Iterating over the contents of the mp3 folder; issue is first item in folder is hidden (.) file, so we need to #select only mp3 files.
-    Dir.entries(@path).select {|file| @files << file.include?(".mp3")}
+    Dir.entries(@path).select {|file| 
+      if file.include?(".mp3") == true
+        @files << file
+      }
   binding.pry
   end
 
